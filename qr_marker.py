906bcecb7ef6410785f22040ae776bc532ff0837
Dominik Rumian <435378@mail.muni.cz>
2020-10-27 21:01:46 +0100

    code adjusted for openCV 4.4.0.44

--- qr_marker.py
+++ qr_marker.py
@@ -1,25 +1,25 @@
-import sys
-
 import cv2
 
 # Initialize the webcam as the source of images
 cap = cv2.VideoCapture(0)
 # Initialize the cv2 QRCode detector
 detector = cv2.QRCodeDetector()
-
 data_prev = None
 while True:
     # Taking images from webcam
     _, img = cap.read()
     # Detects QRcode, sets BB and detects data in QR code
     data_new, bbox, _ = detector.detectAndDecode(img)
-    image = cv2.putText(img, 'Press \'q\' to quit', (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
-                        1, (0, 0, 255), 2, cv2.LINE_AA)
+    image = cv2.putText(img, 'Press \'q\' to quit', (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     # Checks if there is a QRCode in the image
     if bbox is not None:
-        for i in range(len(bbox)):
+        for i in range(len(bbox[0])):
             # Draws BB
-            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i + 1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+
+            # For testing purposes - finding out how data is represented
+            # print(bbox[0])
+            # print(tuple(bbox[0][i]), tuple(bbox[0][(i + 1) % len(bbox)]))
+            cv2.line(img, tuple(bbox[0][i]), tuple(bbox[0][(i + 1) % len(bbox[0])]), color=(255, 63, 111), thickness=2)
         # Avoiding duplicities in printing out encoded data
         if data_new and data_new != data_prev:
             print("Data encoded: ", data_new)
