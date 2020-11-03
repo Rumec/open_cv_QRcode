05bc618b1072f4a3c8804a250621687ea9bb882c
rumec <drumian@seznam.cz>
2020-11-03 12:20:45 +0100

    BBox added to pyzbar and detection improved

--- QR_reader_pyzbar.py
+++ QR_reader_pyzbar.py
@@ -12,22 +12,26 @@ from pyzbar.pyzbar import decode
 
 # Initialize the webcam as the source of images
 cap = cv2.VideoCapture(0)
-count = 0
 data_prev = None
 while True:
 
     # Taking images from webcam
     _, img = cap.read()
 
-    image = cv2.putText(img, 'Press \'q\' to quit', (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-
-    for barcode in decode(image):
+    for barcode in decode(img):
         # Parsing out decoded data
-        data = str(barcode.data).split('\'')
-        print(data[1])
-        print(count)
-        count += 1
+        print(str(barcode.data)[1:])
 
+        ####################################################################################
+        # Following code is only for testing purposes, remove if you need better performance
+        for i in range(len(barcode.polygon)):
+            # Draws BB
+            cv2.line(img, barcode.polygon[i], barcode.polygon[(i + 1) % len(barcode.polygon)], color=(255, 63, 111), thickness=2)
+        image = cv2.putText(img, 'QR code detected!', (340, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
+                            cv2.LINE_AA)
+        ####################################################################################
+
+    image = cv2.putText(img, 'Press \'q\' to quit', (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.imshow("QR code reader", img)
     if cv2.waitKey(1) == ord("q"):
         break
